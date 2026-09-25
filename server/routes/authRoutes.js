034047688c0b7803dfc51c8f093c358fd962f2c8
KatsHue <katia.shuerta@gmail.com>
2026-09-24 23:30:00 -0600

    correción login

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -9,6 +9,11 @@ const router = express.Router();
 router.post('/register', authLimiter, registerValidators, register);
 router.post('/login', authLimiter, loginValidators, login);
 router.post('/logout', logout);
+router.get('/test-me', (req, res) => {
+  res.status(200).json({
+    message: 'AUTH ROUTES FUNCIONA'
+  });
+});
 router.get('/me', protect, getMe);
 
 module.exports = router;
